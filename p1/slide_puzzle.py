35d7f91465a125718997a1429f9400019feb0965
Sarah Cooley <sarah.eleni.cooley@gmail.com>
2011-04-13 10:15:01 -0700

    stuff

--- p1/slide_puzzle.py
+++ p1/slide_puzzle.py
@@ -1,5 +1,8 @@
 import csv
 import sys
+import copy
+
+checked = []
 
 class Node:
 	def __init__(self, parent, action, state, path_cost, depth):
@@ -8,6 +11,9 @@ class Node:
 		self.state = state
 		self.cost = path_cost
 		self.depth = depth
+	
+	def __str__(self):
+		return self.action + " "+str(self.state)
 
 def parseFile(file):
 	out = []
@@ -17,24 +23,104 @@ def parseFile(file):
 		out.append(row)
 
 	return out	
-	
+
+def expand(node):
+	successors = []
+
+	for result, action in suc_fun(node):
+		if result in checked:
+			return successors
+		new_node = Node(node, action, result, node.cost+1, node.depth+1)
+		#print new_node
+		successors.append(new_node)
+	#print successors
+	return successors
+
+def suc_fun(node):
+	state = node.state
+	results = []
+	loc = []
+	for i in range(3):
+		for j in range(3):
+			if state[i][j] == '0':
+				loc = [i, j]
+				#print loc
+	new_state = copy.deepcopy(state)
+	i = loc[0]
+	j = loc[1]
+	#print state
+	if j-1 >= 0:
+		new_state[i][j] = new_state[i][j-1]
+		new_state[i][j-1] = '0'
+		results.append((new_state, str(i) + ", " + str(j-1) + " to " + str(i) + ", " + str(j)))
+		#print "Appending1: " + str(new_state)
+		new_state = copy.deepcopy(state)
+		#print state
+		#print new_state
+	if j+1 <= 2:
+		new_state[i][j] = new_state[i][j+1]
+		new_state[i][j+1] = '0'
+		results.append((new_state, str(i) + ", " + str(j+1) + " to " + str(i) + ", " + str(j)))
+		#print "Appending2: " + str(new_state)
+		new_state = copy.deepcopy(state)
+	if i-1 >= 0:
+		new_state[i][j] = new_state[i-1][j]
+		new_state[i-1][j] = '0'
+		results.append((new_state, str(i+1) + ", " + str(j) + " to " + str(i) + ", " + str(j)))
+		#print "Appending3: " + str(new_state)
+		new_state = copy.deepcopy(state)
+	if i+1 <= 2:
+		new_state[i][j] = new_state[i+1][j]
+		new_state[i+1][j] = '0'
+		results.append((new_state, str(i+1) + ", " + str(j) + " to " + str(i) + ", " + str(j)))
+		#print "Appending4: " + str(new_state)
+		new_state = copy.deepcopy(state)
+
+	#print results
+	return results
+
+def bfs(s, e):
+	n = Node(None, "bfs_init", s, 0, 1)
+	print n
+
+	fringe = []
+	fringe.append(n)
+	while fringe != []:
+		node = fringe.pop(0)
+		print node
+		if node.state in checked:
+			print "Checked"
+			continue
+		if node.state == e:
+			print "Goal"
+			return node
+		else:
+			checked.append(node.state)
+			ex = []
+			for exp in expand(node):
+				print exp
+				ex = [exp] + ex
+			fringe = ex + fringe
+			print "Fringe: "
+			print fringe
+	return None
+
+def dfs(s, e):
+	n = Node(None, "Initial", s, 0, 1)
+	return 0
+
+
 
 start = parseFile(sys.argv[1])
 end = parseFile(sys.argv[2])
 mode = sys.argv[3]
 
-print start
-print end
-print mode
-
-init_node = Node(None, "Initial", initial_state, 0, 1)
-
 if mode == "bfs":
-	node = bfs(init_node, goal_state)
+	node = bfs(start, end)
 elif mode == "dfs":
-	node = dfs(init_node, goal_state)
-print node.state
+	node = dfs(start, end)
 
+print node.state
 while node != None:
 	print node.state
 	node = node.parent
