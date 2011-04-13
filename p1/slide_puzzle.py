6302071b2a078f7e9a0e7b7d478bfe31c9226b14
Sarah Cooley <sarah.eleni.cooley@gmail.com>
2011-04-13 14:55:08 -0700

    move order fixed

--- p1/slide_puzzle.py
+++ p1/slide_puzzle.py
@@ -23,10 +23,8 @@ class Node:
 def parseFile(file):
 	out = []
 	reader = csv.reader(open(file, 'rb'))
-
 	for row in reader:
 		out.append(row)
-
 	return out	
 
 def expand(node):
@@ -39,7 +37,7 @@ def expand(node):
 	return successors
 
 def s_gen(node):
-	moves = ((1,0),(0,1),(-1,0),(0,-1))
+	moves = ((-1,0),(1,0),(0,-1),(0,1))
 	results = []
 	loc = (0,0)
 
@@ -47,7 +45,6 @@ def s_gen(node):
 		for y in range(m):
 			if node.state[x][y] == '0':
 				loc = (x,y)
-
 	x,y = loc
 	possible = map(lambda (i, j): (x + i, y + j), moves)
 	possible = filter(lambda (i, j): 0 <= i < n and 0 <= j < m, possible)
@@ -67,13 +64,13 @@ def bfs(s, e):
 	fringe.append(n)
 	while fringe != []:
 		node = fringe.pop(0)
-		
 		checked.append(node.state)
 		for ex in expand(node):
 			if ex.state == e:
 				print "Goal!"
 				return ex
 			fringe.append(ex)
+
 	print "No Goal :("
 	return None
 
@@ -84,7 +81,6 @@ def dfs(s, e):
 	fringe.append(n)
 	while fringe != []:
 		node = fringe.pop(0)
-		
 		checked.append(node.state)
 		for ex in expand(node):
 			if ex.state == e:
@@ -95,7 +91,8 @@ def dfs(s, e):
 	print "No Goal :("
 	return None
 
-
+def iddfs(s, e):
+	return None
 
 start = parseFile(sys.argv[1])
 end = parseFile(sys.argv[2])
